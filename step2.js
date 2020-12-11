c00f1e73a7ee32919a4f5206c15c70de55833f31
Hongbi <dyongdi@gmail.com>
2020-12-11 14:42:16 +0900

    feat: rotHorizontal 함수 생성
    2차원 배열과 행(가로)의 인덱스를 받아 해당 행을 왼쪽으로 한 칸 미는 함수

--- step2.js
+++ step2.js
@@ -1,6 +1,6 @@
 // ---------------------● 2단계: 평면 큐브 구현하기 ●---------------------
 
-let planeCube = [
+const planeCube = [
   ['R', 'R', 'W'], 
   ['G', 'C', 'W'], 
   ['G', 'B', 'B']
@@ -11,6 +11,33 @@ function rotL(array) {
   const firstElem = dupArr.shift();
   return [...dupArr, firstElem];
 }
+
+// const command = {
+//   'U': ,
+//   'U\'': ,
+//   'R': ,
+//   'R\'': ,
+//   'L': ,
+//   'L\'':
+//   'D': ,
+//   'D\'': ,
+// }
+
+function rotHorizontal(planeCube, rowIndex) {
+  let dupPlaneCube = [...planeCube.map(e => [...e])];
+  let row = dupPlaneCube[rowIndex];
+  row = rotL(row);
+  dupPlaneCube[rowIndex] = row;
+  return dupPlaneCube;
+}
+
+function rotVertical(planeCube, colIndex) {
+  
+}
+
+// ---------------------● test ●---------------------
+
+console.log(rotHorizontal(planeCube, 2));
 
 // ---------------------● node.js 입력 받기 ●---------------------
 
